29fa3beb57b5f2d8fc8d0d8b51e98d7fdcba260d
peepbii10 <pepbiel2003@gmail.com>
2024-05-09 22:43:49 +0200

    canvis

--- Practica_PROLOG/practica.pl
+++ Practica_PROLOG/practica.pl
@@ -1,25 +1,23 @@
 % ?- ciutats([2,2,1,3], [3,1,3,2], [3,1,2,2], [2,3,1,2], [F1,F2,F3,F4]).
 
-ciutats(CL, CR, FH, FL, L) :-    columnaLeft(CL), columnaRight(CR), filaHigh(FH), filaLow(FL), solucio(L).
+ciutats(CL, CR, FH, FL, L) :-  L = [F1, F2, F3, F4],  columnaLeft(CL, L), columnaRight(CR, L), filaHigh(FH, L), filaLow(FL, L).
 
-solucio([_, _, _, _]).
+columnaLeft([A, B, C, D], L1) :- A = 1, L1 = [[4, _, _, _], _, _, _],
+                            B = 1, L1 = [_, [4, _, _, _], _, _],
+                            C = 1, L1 = [_, _, [4, _, _, _], _],
+                            D = 1, L1 = [_, _, _, [4, _, _, _]].
 
-columnaLeft([A, B, C, D]) :- A = 1, L1 = [[4, _, _, _], _, _, _], solucio(L1),
-                            B = 1, L1 = [_, [4, _, _, _], _, _], solucio(L1),
-                            C = 1, L1 = [_, _, [4, _, _, _], _], solucio(L1),
-                            D = 1, L1 = [_, _, _, [4, _, _, _]], solucio(L1).
+columnaRight([A, B, C, D], L2) :- A = 1, L2 = [[_ , _, _, 4], _, _, _],
+                            B = 1, L2 = [_, [_ , _, _, 4], _, _],
+                            C = 1, L2 = [_, _, [_ , _, _, 4], _],
+                            D = 1, L2 = [_, _, _, [_ , _, _, 4]].
 
-columnaRight([A, B, C, D]) :- A = 1, L2 = [[_ , _, _, 4], _, _, _], solucio(L2),
-                            B = 1, L2 = [_, [_ , _, _, 4], _, _], solucio(L2),
-                            C = 1, L2 = [_, _, [_ , _, _, 4], _], solucio(L2),
-                            D = 1, L2 = [_, _, _, [_ , _, _, 4]], solucio(L2).
+filaHigh([A, B, C, D], L3) :- A = 1, L3 = [[4, _, _, _], _, _, _],
+                            B = 1, L3 = [[_, 4, _, _], _, _, _],
+                            C = 1, L3 = [[_, _, 4, _], _, _, _],
+                            D = 1, L3 = [[_, _, _, 4], _, _, _].
 
-filaHigh([A, B, C, D]) :- A = 1, L3 = [[4, _, _, _], _, _, _], solucio(L3),
-                            B = 1, L3 = [[_, 4, _, _], _, _, _], solucio(L3),
-                            C = 1, L3 = [[_, _, 4, _], _, _, _], solucio(L3),
-                            D = 1, L3 = [[_, _, _, 4], _, _, _], solucio(L3).
-
-filaLow([A, B, C, D]) :- A = 1, L4 = [_, _, _, [4, _, _, _]], solucio(L4),
-                          B = 1, L4 = [_, _, _, [_, 4, _, _]], solucio(L4),
-                          C = 1, L4 = [_, _, _, [_, _, 4, _]], solucio(L4),
-                          D = 1, L4 = [_, _, _, [_, _, _, 4]], solucio(L4).
+filaLow([A, B, C, D], L4) :- A = 1, L4 = [_, _, _, [4, _, _, _]],
+                          B = 1, L4 = [_, _, _, [_, 4, _, _]],
+                          C = 1, L4 = [_, _, _, [_, _, 4, _]],
+                          D = 1, L4 = [_, _, _, [_, _, _, 4]].
